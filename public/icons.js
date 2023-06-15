bfbb930e577474eef591dc6b31292f55644185ac
Dipannita Sen <diyasen@pitzer-rw02.ten.osc.edu>
2023-06-15 16:31:59 -0400

    step 4

--- public/icons.js
+++ public/icons.js
@@ -3,12 +3,23 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 */
+// randomize icons and finish later
+function all_icons() {
+    return [...document.styleSheets].flatMap(styleSheet => {
+      return [...styleSheet.cssRules]
+        .map((rule) => {  
+          const m = rule.cssText.match(/^\.fa-([\w-]+)/);
+          if(m && m[1]) {
+            let b = 'break!';
+            $("#icon_test").append(`<p>${m[1]}</p>`);
+            return m[1];
+          }
+        })
+        .filter(r => r);
+      });
+    }
 
-function allIcons()
-{
-    return [...document.styleSheets].flatMap(styleSheet=>
-        {
-            return[...styleSheet.cssRules]
-            
-        }
-}
+    const ALL_ICONS =
+    [
+
+    ]
