e7a090a2d27239bd954ca7fafc466cc46ee9f2cf
Dipannita Sen <diyasen@pitzer-rw02.ten.osc.edu>
2023-06-16 15:04:19 -0400

    submitting the job

--- public/icons.js
+++ public/icons.js
@@ -3,23 +3,4 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 */
-// randomize icons and finish later
-function all_icons() {
-    return [...document.styleSheets].flatMap(styleSheet => {
-      return [...styleSheet.cssRules]
-        .map((rule) => {  
-          const m = rule.cssText.match(/^\.fa-([\w-]+)/);
-          if(m && m[1]) {
-            let b = 'break!';
-            $("#icon_test").append(`<p>${m[1]}</p>`);
-            return m[1];
-          }
-        })
-        .filter(r => r);
-      });
-    }
-
-    const ALL_ICONS =
-    [
-
-    ]
+//ability to choose icon
